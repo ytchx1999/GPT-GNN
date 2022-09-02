d70c0da944f6776c08a4b0d390dbd4d3d321604f
ytchx1999 <54234005+ytchx1999@users.noreply.github.com>
2022-09-02 17:17:33 +0800

    add scripts for time_field

--- example_amazon_gowalla/scripts/time_field/fintune_gowalla_nightlife_gpt.sh
+++ example_amazon_gowalla/scripts/time_field/fintune_gowalla_nightlife_gpt.sh
@@ -3,7 +3,7 @@ cd "$(dirname $0)"
 python3 ../../finetune_gpt.py \
 -d gowalla_Nightlife \
 --data_type gowalla \
---task_type field_trans \
+--task_type tf_trans \
 --use_pretrain \
 --n_epoch 40 \
 --cuda 1 \
